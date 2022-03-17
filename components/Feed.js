8e9e7168ec208d16b1c312430db93ca9243ac0b1
AGH <rlghks9252@gmail.com>
2022-03-17 17:57:28 +0900

    facebook clone (next.js, tailwind)

---
+++ components/Feed.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Stories from './Stories'
+function Feed() {
+  return (
+    <div className='flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto'>
+      <div>
+        <Stories />
+        {/* InputBox */}
+        {/* Posts */}
+      </div>
+    </div>
+  )
+}
+
+export default Feed
