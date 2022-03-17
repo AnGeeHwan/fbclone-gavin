a5566b97f7739721b85a19435d89368fa06c5114
AnGeeHwan <rlghks9252@gmail.com>
2022-03-18 01:28:04 +0900

    Add InputBox feed insert window

--- components/Feed.js
+++ components/Feed.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Stories from "./Stories";
-import InputBox from "./Input";
+import InputBox from "./InputBox";
 
 function Feed() {
   return (
     <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
-      <div>
+      <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
         <Stories />
         <InputBox />
         {/* Posts */}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,22 +1,22 @@
-import { useSession } from 'next-auth/react'
-import React from 'react'
+import { useSession } from "next-auth/react";
+import React from "react";
 import {
   ChevronDownIcon,
   ShoppingCartIcon,
   UserGroupIcon,
-} from "@heroicons/react/outline"
+} from "@heroicons/react/outline";
 import {
   CalendarIcon,
   ClockIcon,
   DesktopComputerIcon,
   UsersIcon,
-} from "@heroicons/react/solid"
-import SidebarRow from './SidebarRow'
+} from "@heroicons/react/solid";
+import SidebarRow from "./SidebarRow";
 
 function Sidebar() {
   const { data: session, loading } = useSession();
   return (
-    <div className='p-2 mt-5 max-w-[600px] xl:min-w-[300px]'>
+    <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
       <SidebarRow src={session.user.image} title={session.user.name} />
       <SidebarRow Icon={UsersIcon} title="Friends" />
       <SidebarRow Icon={UserGroupIcon} title="Groups" />
@@ -26,7 +26,7 @@ function Sidebar() {
       <SidebarRow Icon={UsersIcon} title="Memories" />
       <SidebarRow Icon={ChevronDownIcon} title="See More" />
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
